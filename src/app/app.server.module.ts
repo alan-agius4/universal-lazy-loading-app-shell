05ddea1c8bf8100adc8779d625371d214f525b31
Alan Agius <alan.agius4@gmail.com>
2018-09-07 11:21:26 +0200

    initial

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,9 +1,13 @@
-import {NgModule} from '@angular/core';
-import {ServerModule, ServerTransferStateModule} from '@angular/platform-server';
-import {ModuleMapLoaderModule} from '@nguniversal/module-map-ngfactory-loader';
+import { NgModule, NgModuleFactoryLoader } from '@angular/core';
+import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
+import { Routes, RouterModule } from '@angular/router';
+import { ModuleMapLoaderModule, ModuleMapNgFactoryLoader } from '@nguniversal/module-map-ngfactory-loader';
 
-import {AppModule} from './app.module';
-import {AppComponent} from './app.component';
+import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
+import { AppShellComponent } from './app-shell/app-shell.component';
+
+const routes: Routes = [{ path: 'shell', component: AppShellComponent }];
 
 @NgModule({
   imports: [
@@ -12,10 +16,16 @@ import {AppComponent} from './app.component';
     AppModule,
     ServerModule,
     ModuleMapLoaderModule,
+    RouterModule.forRoot(routes),
     ServerTransferStateModule,
   ],
+  providers: [{
+    provide: NgModuleFactoryLoader,
+    useClass: ModuleMapNgFactoryLoader
+  }],
   // Since the bootstrapped component is not inherited from your
   // imported AppModule, it needs to be repeated here.
   bootstrap: [AppComponent],
+  declarations: [AppShellComponent],
 })
-export class AppServerModule {}
+export class AppServerModule { }
